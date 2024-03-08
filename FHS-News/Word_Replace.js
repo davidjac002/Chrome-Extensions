cd0d5c51143a0aaea7ac735aa1612d2221612a22
Jackson David <97686021+davidjac002@users.noreply.github.com>
2024-03-08 08:41:52 -0500

    first commit

---
+++ FHS-News/Word_Replace.js
@@ -0,0 +1,8 @@
+const text = document.querySelectorAll("h1, h2, h3 , h4 , h5 , p , li , td , caption , span , a");
+for (let i = 0; i < text.length; i++) {
+ 
+  if(text[i].innerHTML.includes('Dog')){
+    text[i].innerHTML = text[i].innerHTML.replace('Dog','God')
+  }
+
+}
