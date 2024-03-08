e25bc0329ffeaeeb45f4470b070a6f0d287bc43f
Jackson David <97686021+davidjac002@users.noreply.github.com>
2024-03-08 08:53:25 -0500

    Changed Capitalization of Dog in order to

--- FHS-News/Word_Replace.js
+++ FHS-News/Word_Replace.js
@@ -1,8 +1,8 @@
 const text = document.querySelectorAll("h1, h2, h3 , h4 , h5 , p , li , td , caption , span , a");
 for (let i = 0; i < text.length; i++) {
  
-  if(text[i].innerHTML.includes('Dog')){
-    text[i].innerHTML = text[i].innerHTML.replace('Dog','God')
+  if(text[i].innerHTML.includes('dog')){
+    text[i].innerHTML = text[i].innerHTML.replace('dog','God')
   }
 
 }
